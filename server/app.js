135e99ba397c7759aa12bd8ac54e6ee22f5637bc
Sam Heckle <samheckle@yahoo.com>
2018-10-24 22:27:17 -0400

    finished middleware

--- server/app.js
+++ server/app.js
@@ -7,6 +7,8 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const url = require('url');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
@@ -17,6 +19,18 @@ mongoose.connect(dbURL, (err) => {
     throw err;
   }
 });
+
+let redisURL = {
+  hostname: 'localhost',
+  port: 6379,
+};
+
+let redisPASS;
+
+if (process.env.REDISCLOUD_URL) {
+  redisURL = url.parse(process.env.REDISCLOUD_URL);
+  redisPASS = redisURL.auth.split(':')[1];
+}
 
 const router = require('./router.js');
 
@@ -29,6 +43,11 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(session({
   key: 'sessionid',
+  store: new RedisStore({
+    host: redisURL.hostname,
+    port: redisURL.port,
+    pass: redisPASS,
+  }),
   secret: 'Domo Arigato',
   resave: true,
   saveUninitialized: true,
